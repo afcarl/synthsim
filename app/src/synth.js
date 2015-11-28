588982f3a7eb6adb156b889df87c6b8d7e048c68
davidlukerice <davidlukerice@gmail.com>
2015-11-27 19:50:17 -0600

    breaking off sim into its own class

---
+++ app/src/synth.js
@@ -0,0 +1,62 @@
+
+
+function getNearestPellet(game, foodGroup, synthSprite) {
+  var pellet = null;
+  var distance = Number.MAX_VALUE;
+  foodGroup.forEach((newPellet) => {
+    var newDistance = game.physics.arcade.distanceBetween(synthSprite, newPellet);
+    if (newDistance < distance) {
+      distance = newDistance;
+      pellet = newPellet;
+    }
+  }, this, true);
+  return pellet;
+}
+
+export default class {
+  constructor(game) {
+    this.game = game;
+    this.sprite = null;
+    this.cursors = null;
+  }
+  preload() {
+    this.game.load.spritesheet('synth', 'assets/bullet.png', 39, 39);
+  }
+  create() {
+    this.sprite = this.game.add.sprite(0, 0, 'synth');
+    this.game.physics.arcade.enable(this.sprite);
+    this.sprite.body.bounce.y = 0.2;
+    this.sprite.body.gravity.y = 0;
+    this.sprite.body.collideWorldBounds = true;
+
+    this.cursors = this.game.input.keyboard.createCursorKeys();
+  }
+  update(food) {
+    var closestPellet = getNearestPellet(this.game, food, this.sprite);
+    if (closestPellet) {
+      this.game.physics.arcade.moveToObject(this.sprite, closestPellet, 50);
+    }
+    else {
+      this.sprite.body.velocity.x = 0;
+      this.sprite.body.velocity.y = 0;
+    }
+
+    if (this.cursors.left.isDown)
+    {
+        this.sprite.body.velocity.x = -150;
+    }
+    else if (this.cursors.right.isDown)
+    {
+        this.sprite.body.velocity.x = 150;
+    }
+
+    if (this.cursors.up.isDown)
+    {
+        this.sprite.body.velocity.y = -150;
+    }
+    else if (this.cursors.down.isDown)
+    {
+        this.sprite.body.velocity.y = 150;
+    }
+  }
+};
--- app/src/synthSim.js
+++ app/src/synthSim.js
@@ -1,17 +1,19 @@
 
+import Synth from './synth';
+
 var GAME_WIDTH = 800;
 var GAME_HEIGHT = 600;
 var game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
-var synth, world, food, mod;
+var synth = new Synth(game);
+var world, food, mod;
 
 var collected = 0;
 var collectedText = "";
 
-var cursors;
-
 function preload() {
-  game.load.spritesheet('bullet', 'assets/bullet.png', 39, 39);
+  this.game.load.spritesheet('bullet', 'assets/bullet.png', 39, 39);
+  synth.preload();
 }
 
 function create() {
@@ -20,11 +22,7 @@ function create() {
   world = game.add.group();
   world.enableBody = true;
 
-  synth = game.add.sprite(0, 0, 'bullet');
-  game.physics.arcade.enable(synth);
-  synth.body.bounce.y = 0.2;
-  synth.body.gravity.y = 0;
-  synth.body.collideWorldBounds = true;
+  synth.create();
 
   food = game.add.group();
   food.enableBody = true;
@@ -40,57 +38,17 @@ function create() {
   mod.body.immovable = true;
 
   collectedText = game.add.text(16, 16, 'Collected: 0', { fontSize: '32px', fill: '#fff' });
-  cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
-  game.physics.arcade.collide(synth, world);
-  game.physics.arcade.overlap(synth, food, function(synth, food) {
+  game.physics.arcade.collide(synth.sprite, world);
+  game.physics.arcade.overlap(synth.sprite, food, function(synthSprite, food) {
     food.kill();
     collected++;
     collectedText.text = 'Collected: ' + collected;
   }, null, this);
 
-
-  var closestPellet = getNearestPellet();
-  if (closestPellet) {
-    game.physics.arcade.moveToObject(synth, closestPellet, 50);
-  }
-  else {
-    synth.body.velocity.x = 0;
-    synth.body.velocity.y = 0;
-  }
-
-  if (cursors.left.isDown)
-  {
-      synth.body.velocity.x = -150;
-  }
-  else if (cursors.right.isDown)
-  {
-      synth.body.velocity.x = 150;
-  }
-
-  if (cursors.up.isDown)
-  {
-      synth.body.velocity.y = -150;
-  }
-  else if (cursors.down.isDown)
-  {
-      synth.body.velocity.y = 150;
-  }
-}
-
-function getNearestPellet() {
-  var pellet = null;
-  var distance = Number.MAX_VALUE;
-  food.forEach((newPellet) => {
-    var newDistance = game.physics.arcade.distanceBetween(synth, newPellet);
-    if (newDistance < distance) {
-      distance = newDistance;
-      pellet = newPellet;
-    }
-  }, this, true);
-  return pellet;
+  synth.update(food);
 }
 
 export default game;
