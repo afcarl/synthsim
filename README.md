10ceb7538a697e64eeafb281e4e1578dbf7e0cda
davidlukerice <davidlukerice@gmail.com>
2015-11-27 19:32:58 -0600

    better build system with babel/browserify

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # synthsim
 SynthSim is an audioscape generator utilizing various simulations
 
-## Building
-  1. Build everything with `gulp`
-  2. Then host the `dist` folder with something like `http-server`
+## Building/Running
+  1. Install deps with `npm install`
+  2. Build everything with `gulp`
+  3. Then host the `dist` folder with something like `http-server`
+  4. Goto the hosted website in a browser. ie `localhost:8080`
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,56 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
 
-gulp.task('default', function() {
+var gulp = require('gulp'),
+    babelify = require('babelify'),
+    watch = require('gulp-watch'),
+    watchify = require('watchify'),
+    sourcemaps = require('gulp-sourcemaps');
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    del = require('del');
+
+gulp.task('clean-temp', function(){
+  return del(['dist']);
+});
+
+gulp.task('assets', ['clean-temp'], function() {
+  // Copy over the required dependencies
   gulp.src('./node_modules/phaser/dist/phaser.js')
     .pipe(gulp.dest('./dist/lib'));
+
+  // Copy over the game assets
   gulp.src('./assets/*')
       .pipe(watch('./assets/*'))
       .pipe(gulp.dest('./dist/assets'));
-  gulp.src('./app/*')
-      .pipe(watch('./app/*'))
+});
+
+gulp.task('html', ['clean-temp'], function() {
+  return gulp.src('./app/index.html')
+      .pipe(watch('./app/index.html'))
       .pipe(gulp.dest('./dist'));
 });
+
+gulp.task('src', ['clean-temp'], function(){
+
+  var bundler = watchify(
+    browserify('./app/index.js', { debug: true })
+      .transform(babelify, {presets: ['es2015']})
+  ).on('update', function() {
+    console.log('-> bundling...');
+    rebundle();
+  });
+
+  function rebundle() {
+    bundler.bundle()
+      .on('error', function(err) { console.error(err); this.emit('end'); })
+      .pipe(source('index.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('./dist'));
+  }
+
+  rebundle();
+});
+
+gulp.task('default', ['clean-temp', 'assets', 'html', 'src']);
